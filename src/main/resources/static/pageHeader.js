4df677951a1b0cd0a420eb634cd032accfdf659c
Evgeny Roznin <real282@mail.ru>
2021-09-30 16:47:53 +0800

    new user, не работает 3

--- src/main/resources/static/pageHeader.js
+++ src/main/resources/static/pageHeader.js
@@ -1,11 +1,6 @@
 fetch("/api/getAuthorizedUser").then((response) => {
     console.log("PAGE HEADER!")
     response.json().then((user) => {
-        let rolesUserI = user.roles
-        let strRoles = ""
-        for (let j = 0; j < rolesUserI.length; j++) {
-            strRoles += rolesUserI[j].role + " "
-        }
 
     })
 })
