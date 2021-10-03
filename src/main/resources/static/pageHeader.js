fb3fb0e886a6fcd4e2f0596e838b1c1a3e3869e6
Admin <real282@mail.ru>
2021-10-03 14:06:54 +0800

    На проверку

--- src/main/resources/static/pageHeader.js
+++ src/main/resources/static/pageHeader.js
@@ -1,6 +1,12 @@
 fetch("/api/getAuthorizedUser").then((response) => {
     console.log("PAGE HEADER!")
     response.json().then((user) => {
-
+        document.getElementById("headerMail").innerText = user.email
+        let rolesUser = user.roles
+        let strRoles=""
+        for (let j = 0; j < rolesUser.length; j++) {
+            strRoles += rolesUser[j].role + " "
+        }
+        document.getElementById("headerRole").innerText = strRoles
     })
 })
